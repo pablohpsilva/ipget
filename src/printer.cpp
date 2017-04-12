44df8aa66de7805845c1752d1d7509f91206e28a
Jefferson Puchalski <puchiruzuki@gmail.com>
2017-04-12 15:38:00 -0300

    Add _GUI_ test window in GKT+

--- src/printer.cpp
+++ src/printer.cpp
@@ -283,3 +283,27 @@ bool PortCheck::CheckPort(char* host, int port)
     return true;
    
 }
+
+
+#if _GUI_
+/**
+ *
+ * Gui Init for window. 
+ *
+ */
+
+void GUI::createFrame()
+{
+
+}
+
+void GUI::static void activate (GtkApplication *app, gpointer user_data)
+{
+	GtkWidget *window;
+
+	  window = gtk_application_window_new (app);
+  	 gtk_window_set_title (GTK_WINDOW (window), "Window");
+ 	 gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+	 gtk_widget_show_all (window);
+}
+#endif
